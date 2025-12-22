7da0c3fc7018a2402c6da1e29de84e6bdbe3f471
nj <jainnamrata722@gmail.com>
2025-12-22 19:23:16 +0530

    Refine Preprocessing logic

--- train_and_save.py
+++ train_and_save.py
@@ -171,6 +171,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 # Scaling
+#ensure equal contribution
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
